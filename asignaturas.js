fea4e7698207305cc6c9b80ab94097d0951560c6
lagatta <50701514+lagatta@users.noreply.github.com>
2019-06-24 13:52:50 +0200

    Add files via upload

---
+++ asignaturas.js
@@ -0,0 +1,80 @@
+var runners = ["pedro" , "maria" , "sofia" ,"javi" , "daniel" , "ariel","tenefe"];
+
+var i;
+var text = "";
+for (i = 0; i < 5; i++){
+    text += runners[i] + "<br>";
+}
+document.write(text);
+var runners = ["sofia" ," carlo" ,"salit" , "sandra" ,"domingo" ,"fff"];
+
+i = 0;
+text = "";
+
+while( i < 5) {
+    text += runners[i] + "<br>";
+    i++;
+}
+document.write(text);
+var runners = ["tonto" ,"sal" ,"tutut" , "sandra" , "tatat",];
+var text = "";
+var i = 0;
+do{
+    text += runners[i] + "<br>";
+    i++;
+} while (i < 5);
+document.write(text);
+//Construye un sistema que nos muestra por
+//pantalla todos los números del 1 al 10. Construyelo
+//de las 3 maneras vista en el día.//
+
+var number = [1,2,3,4,5,6,7,8,9,10];
+var text_7 = "";
+var u = 0;
+for (u = 0; u < 10; u++){
+    text_7 += number[u] + "<br>";
+}
+document.write(text_7);
+var number = [1,2,3,4,5,6,7,8,9,10];
+var text_7 = "";
+var u = 0;
+while( u < 10) {
+    text_7 += number[u] + "<br>";
+    u++;
+}{
+document.write(text_7);
+}
+do {
+    document.write(number[u]* 2);
+    u++
+}while(u<number.length);
+{document.write(text_7);
+}
+
+var runners = ["pedro" , "sandra" , "fofia" , "zaida" , "carmen"];
+var i;
+text = "";
+for (i = 0; i < 5; i++){
+    if (i == 3) {
+        continue;
+    }
+text += runners[i] + "<br>";
+}
+document.write(text);
+//Obtén el número total de alumnos por asignatura, el nombre de la asignatura y el
+//nombre y apellidos del profesor que la imparte.//
+
+var asignatura = ["matematicas" , "lengua" , "sociales" , "fisica" , "diseño" , "educacion" , "etica" , "programacion"];
+
+//numero de alumnos por asignaturas//
+var y;(0>0)
+// nombres y apellidos del preofesor que imparte la asignatura//
+var p_1 = "";
+for [{y ==
+
+    p_1 + = asignatura[y] + "<br>";
+}
+.toUpperCase()
+
+
+
